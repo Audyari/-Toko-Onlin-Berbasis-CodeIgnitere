a826c332add857899430903c0e6108b7c013ce8f
Audyari Wiyono <audy123ari@gmail.com>
2024-06-22 22:26:28 +0700

    Membuat Fungsi Pagination

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -2,7 +2,10 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 class MY_Model extends CI_Model
 {
+
 	protected $table = '';
+	protected $perPage = 5;
+
 	public function __construct()
 	{
 		parent::__construct();
@@ -28,11 +31,8 @@ class MY_Model extends CI_Model
 		);
 		$validationRules = $this->getValidationRules();
 		$this->form_validation->set_rules($validationRules);
-
 		return $this->form_validation->run();
 	}
-
-
 	/**
 	 * Seleksi data per-kolom
 	 * Chain Method
@@ -45,7 +45,6 @@ class MY_Model extends CI_Model
 		$this->db->select($columns);
 		return $this;
 	}
-
 	/**
 	 * Mencari suatu data pada kolom tertentu dengan data yang sama
 	 * Chain Method
@@ -58,7 +57,6 @@ class MY_Model extends CI_Model
 		$this->db->where($column, $condition);
 		return $this;
 	}
-
 	/**
 	 * Mencari suatu data pada kolom tertentu dengan data yang mirip
 	 * Chain Method
@@ -71,7 +69,6 @@ class MY_Model extends CI_Model
 		$this->db->like($column, $condition);
 		return $this;
 	}
-
 	/**
 	 * Mencari suatu data selanjutnya pada kolom tertentu dengan data yang mirip
 	 * Chain Method
@@ -84,7 +81,6 @@ class MY_Model extends CI_Model
 		$this->db->or_like($column, $condition);
 		return $this;
 	}
-
 	/**
 	 * Menggabungkan Table yang berelasi yang memiliki foreign key id_namatable
 	 * Chain Method
@@ -97,7 +93,6 @@ class MY_Model extends CI_Model
 		$this->db->join($table, "$this->table.id_$table = $table.id", $type);
 		return $this;
 	}
-
 	/**
 	 * Mengurutkan data dari hasil query dan kondisi
 	 * Chain Method
@@ -110,7 +105,6 @@ class MY_Model extends CI_Model
 		$this->db->order_by($column, $order);
 		return $this;
 	}
-
 	/**
 	 * Menampilkan satu data dari hasil query dan kondisi
 	 * Hasil Akhir Chain Method
@@ -120,7 +114,6 @@ class MY_Model extends CI_Model
 	{
 		return $this->db->get($this->table)->row();
 	}
-
 	/**
 	 * Menampilkan banyak data dari hasil query dan kondisi
 	 * Hasil Akhir Chain Method
@@ -130,7 +123,6 @@ class MY_Model extends CI_Model
 	{
 		return $this->db->get($this->table)->result();
 	}
-
 	/**
 	 * Menampilkan nilai jumlah data dari hasil query dan kondisi
 	 * Hasil Akhir Chain Method
@@ -140,7 +132,6 @@ class MY_Model extends CI_Model
 	{
 		return $this->db->count_all_results($this->table);
 	}
-
 	/**
 	 * Menyimpan data baru ke dalam suatu tabel
 	 *
@@ -152,7 +143,6 @@ class MY_Model extends CI_Model
 		$this->db->insert($this->table, $data);
 		return $this->db->insert_id();
 	}
-
 	/**
 	 * Mengubah data yang ada pada suatu tabel dengan data baru
 	 *
@@ -173,6 +163,81 @@ class MY_Model extends CI_Model
 	{
 		$this->db->delete($this->table);
 		return $this->db->affected_rows();
+	}
+
+	/**
+	 * Menentukan Limit data untuk ditampilkan
+	 *
+	 * @param [type] $page
+	 * @return void
+	 */
+	public function paginate($page)
+	{
+		$this->db->limit(
+			$this->perPage,
+			$this->calculateRealOffset($page)
+		);
+	}
+
+	/**
+	 * Menggantikan offset dengan nilai sesuai halaman
+	 *
+	 * @param [type] $page
+	 * @return void
+	 */
+	public function calculateRealOffset($page)
+	{
+		if (is_null($page) || empty($page)) {
+			$offset = 0;
+		} else {
+			$offset = ($page * $this->perPage) - $this->perPage;
+		}
+
+		return $offset;
+	}
+
+	/**
+	 * Membuat Pagination dengan style bootstrap 4
+	 *
+	 * @param [type] $baseUrl
+	 * @param [type] $uriSegment
+	 * @param [type] $totalRows
+	 * @return void
+	 */
+	public function makePagination($baseUrl, $uriSegment, $totalRows = null)
+	{
+		$this->load->libary('pagination');
+
+		$config = [
+			'base_url' => $baseUrl,
+			'uri_segment' => $uriSegment,
+			'per_page' => $this->perPage,
+			'total_rows' => $totalRows,
+			'use_page_numbers' => true,
+
+			'full_tag_open' => '<ul class="pagination">',
+			'full_tag_close' => '</ul>',
+			'attributes' => ['class' => 'page-link'],
+			'first_link' => false,
+			'last_link' => false,
+			'first_tag_open' => '<li class="page-item">',
+			'first_tag_close' => '</li>',
+			'prev_link' => '&laquo',
+			'prev_tag_open' => '<li class="page-item">',
+			'prev_tag_close' => '</li>',
+			'next_link' => '&raquo',
+			'next_tag_open' => '<li class="page-item">',
+			'next_tag_close' => '</li>',
+			'last_tag_open' => '<li class="page-item">',
+			'last_tag_close' => '</li>',
+			'cur_tag_open' => '<li class="page-item active"><a href="#" class="page-link">',
+			'cur_tag_close' => '<span class="sr-only">(current)</span></a></li>',
+			'num_tag_open' => '<li class="page-item">',
+			'num_tag_close' => '</li>',
+		];
+
+		$this->pagination->initialize($config);
+		return $this->pagination->create_link();
 	}
 }
 
